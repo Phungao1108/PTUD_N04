7d6bf4469002c3207c1c3ae6ae86032507fbdf8d
PlictorNguyen <Phuctaaz@gmail.com>
2026-03-29 20:30:35 +0700

    thêm 2 ui

--- src/com/team/invoice/ui/ContentPanel.java
+++ src/com/team/invoice/ui/ContentPanel.java
@@ -20,15 +20,58 @@ public class ContentPanel extends JPanel {
         setBackground(Color.WHITE);
 
         // Đăng ký các màn hình vào CardLayout với một "Từ khóa" (Key)
+package com.team.invoice.ui;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class ContentPanel extends JPanel {
+    private final CardLayout cardLayout;
+    private final Map<String, JComponent> panelMap = new HashMap<>();
+
+    public ContentPanel() {
+        cardLayout = new CardLayout();
+        setLayout(cardLayout);
+        setBackground(Color.WHITE);
+
         addPanel("Tổng quan", new JLabel("Màn hình Dashboard", SwingConstants.CENTER));
-        addPanel("Dịch vụ", new DichvuUI()); 
-        addPanel("Đơn giá", new DonGiaUI());
         addPanel("Loại phòng", new LoaiPhongUI());
         addPanel("Phòng", new PhongUI());
-        addPanel("Bảng giá", new BangGiaUI()); 
+        addPanel("Dịch vụ", new DichvuUI());
+        addPanel("Bảng giá", new BangGiaUI());
         addPanel("Đơn giá", new DonGiaUI());
         addPanel("Hóa đơn", new JLabel("Màn hình Hóa đơn", SwingConstants.CENTER));
+        addPanel("Hợp đồng", new HopDongUI());
+        addPanel("Khách thuê", new KhachThueUI());
         addPanel("Cài đặt", new JLabel("Màn hình Cài đặt", SwingConstants.CENTER));
+    }
+
+    public void addPanel(String name, JComponent panel) {
+        add(panel, name);
+        panelMap.put(name, panel);
+    }
+
+    public void showPanel(ActionEvent e) {
+        String name = e.getActionCommand();
+        cardLayout.show(this, name);
+    }
+
+    public void showPanelByName(String name) {
+        cardLayout.show(this, name);
+    }
+
+    public JComponent getPanel(String name) {
+        return panelMap.get(name);
+    }
+}
     }
 
     public void addPanel(String name, JComponent panel) {
